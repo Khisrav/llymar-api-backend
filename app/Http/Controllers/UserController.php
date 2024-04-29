666376d4dd3dffbc6b99d428c2b19b5d290bdc23
Khisrav <kh.khisrav2018@gmail.com>
2024-04-29 17:35:18 +0300

    Fix: user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+// use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function index() {
-        return User::all();
+        return Auth::user();
     }
 }
